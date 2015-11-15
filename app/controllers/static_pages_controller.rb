e24132694e6f211dd14f76443ff0d1cc12e4f82a
Zane <i.zane.am.robot@gmail.com>
2015-11-15 12:18:39 -0500

    Added my email address

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ def thank_you
   @email = params[:email]
   @message = params[:message]
   ActionMailer::Base.mail(:from => @email,
-        :to => 'your-email@example.com',
+        :to => 'zanesandin@gmail.com',
         :subject => "A new contact form message from #{@name}",
         :body => @message).deliver_now
 end
