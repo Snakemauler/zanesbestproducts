ed24e3d8b7d402d942f8319fcb34e68d7595e33f
Zane <i.zane.am.robot@gmail.com>
2015-11-18 18:56:47 -0500

    Products show page should be functional now.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
+
 	def create
 		@product = Product.find(params[:product_id])
-		@comment = @product.comments.new(params[:comment])
+		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
-		@comment.save
-		redirect_to products_path(@product)
+		respond_to do |format|
+			if @comment.save
+				format.html { redirect_to @product, notice: 'Review was created successfully.'}
+				format.json { render :show, status: :created, location: @product }
+			else
+				format.html { redirect_to @product, notice: 'Review was not saved successfully'}
+				ormat.json { render json: @comment.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 
 	def destroy
