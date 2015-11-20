ceffc2eb53992f24b99492ed5b9224159960cece
Zane <i.zane.am.robot@gmail.com>
2015-11-20 08:22:13 -0500

    fixing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,10 @@ class CommentsController < ApplicationController
 		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
-		respond_to do |format|
-			if @comment.save
-				format.html { redirect_to @product, notice: 'Review was created successfully.'}
-				format.json { render :show, status: :created, location: @product }
-			else
-				format.html { redirect_to @product, notice: 'Review was not saved successfully'}
-				ormat.json { render json: @comment.errors, status: :unprocessable_entity }
-			end
-		end
+		@comment.save
+		redirect_to products_path(@product)
 	end
+
 
 	def destroy
 	end
