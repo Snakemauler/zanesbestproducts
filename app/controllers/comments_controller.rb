decf4a20198b6b18201090a111c713287ce3bc2c
Zane <i.zane.am.robot@gmail.com>
2015-11-22 21:32:16 -0500

    Added admin can manage all to ability rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:user_id, :body, :rating)
+		params.require(:comment).permit(:user_id, :body, :rating, :submit)
 	end
 
 end
