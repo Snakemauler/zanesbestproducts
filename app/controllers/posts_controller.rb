3344334d09853f391f2c71b5df125b610eb39d3b
Zane <i.zane.am.robot@gmail.com>
2015-11-23 21:53:58 -0500

    Fixed glyphicon took out thing from routes and allowed users to read posts in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    if signed_in? && current_user.admin?
+    if signed_in?
       @posts = Post.all
     else
       redirect_to root_path
