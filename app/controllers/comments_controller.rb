0f4db4671b1442d099ecff8c72f9f55434ac3815
Zane <i.zane.am.robot@gmail.com>
2015-11-24 09:21:53 -0500

    Comments box should be functioning now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:user_id, :body, :rating, :submit)
+		params.require(:comment).permit(:user_id, :body, :rating)
 	end
 
 end
